d55e90c4ad40c3207ea41128cdf17b5836792102
navyapatel225-a11y <navyapatel225@gmail.com>
2026-04-08 21:31:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from server.app import app
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return JSONResponse({"message": "App is running 🚀"})
+
+@app.get("/hello")
+def hello(name: str = "User"):
+    return JSONResponse({"message": f"Hello {name}"})
